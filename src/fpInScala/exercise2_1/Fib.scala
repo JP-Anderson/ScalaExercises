ebc9499371c4cae36f430289487b4d9163fb00b6
dahui58 <dahui58@hotmail.com>
2018-11-10 15:49:04 +0000

    Added some comments to fib

--- src/fpInScala/exercise2_1/Fib.scala
+++ src/fpInScala/exercise2_1/Fib.scala
@@ -8,9 +8,20 @@ class Fib {
   def fib(n: Int): Int = {
     @annotation.tailrec
     def loop(n: Int, prev: Int, cur: Int): Int =
+      // Base case, return prev if n less than or equal to 0
       if (n <= 0) prev
+      // Recursive case, new prev is current, new current is prev + cur, decrement n
       else loop(n - 1, cur, prev + cur)
     loop(n, 0, 1)
   }
+
+  /* Stack trace for fib(5)
+    loop(5, 0, 1)
+    loop(4, 1, 1)
+    loop(3, 1, 2)
+    loop(2, 2, 3)
+    loop(1, 3, 5)
+    loop(0, 5, 8) n == 0 -> return prev = 5
+   */
 
 }
