e39fa5d41f6eb6232ae1bafd2b0231fe4a2506c9
dahui58 <dahui58@hotmail.com>
2018-11-10 18:18:41 +0000

    Small improvements to curry example

--- test/fpInScala/exercise2_3/AdditionCurrySpec.scala
+++ test/fpInScala/exercise2_3/AdditionCurrySpec.scala
@@ -4,13 +4,13 @@ import org.scalatest.FlatSpec
 
 class AdditionCurrySpec extends FlatSpec {
 
-  val curry = new AdditionCurry();
+  val curry = new AdditionCurry()
 
-  "curried functions" should "be equivalent to multi-parameter version" in {
+  "longhand and shorthand curries" should "be equivalent to non curried method with same params" in {
     assert(
       curry.uncurriedAdd(1, 1) === curry.longhandCurriedAdd(1)(1)
       && curry.longhandCurriedAdd(1)(1) === curry.shorthandCurriedAdd(1)(1)
     )
   }
-
+  
 }
